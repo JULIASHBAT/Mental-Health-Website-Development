b48c842715859fdc75e24955c4753ceb83a73b2f
Mohameed7993 <hmody.chechareto@gmail.com>
2024-02-04 17:31:49 +0200

    الأحد 04-02-24

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Mohameed7993/templet-web-week3.git
+git remote add origin https://github.com/Mohameed7993/web-hw1.git
 git push -u origin main
