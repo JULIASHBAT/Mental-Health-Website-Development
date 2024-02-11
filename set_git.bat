ed7d8df14349ccf50c0aa1b547ab8873e801d09a
Mohameed7993 <hmody.chechareto@gmail.com>
2024-02-11 18:09:30 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/marysoul99/A14.git
+git remote add origin  https://github.com/marysoul99/A14.git
 git push -u origin main
