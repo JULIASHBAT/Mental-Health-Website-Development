78f73932f58a973ff78f17bbe9494a6daef13e2a
Mohameed7993 <hmody.chechareto@gmail.com>
2024-02-11 18:13:17 +0200

    الأحد 11-02-24

--- set_git.bat
+++ set_git.bat
@@ -1,12 +1,22 @@
-:: this bat file is used to set git repository
-:: to run use .\set_git.bat
 @echo off
-:: add :: before the next row to initialize the repository
-::exit /b
+:: This batch file updates the Git repository remote URL and pushes the changes
+:: To run, use .\set_git.bat
 
-git init
-git add .
-git commit -m "first commit"
+:: Ensure git is initialized; if not, uncomment the next two lines
+:: git init
+:: git add .
+
+:: Commit any changes - modify the commit message as needed
+git commit -m "Update repository"
+
+:: Rename the default branch to main if it's not already set
 git branch -M main
-git remote add origin  https://github.com/marysoul99/A14.git
+
+:: Remove the old remote URL (if exists)
+git remote remove origin
+
+:: Add the new remote repository URL
+git remote add origin https://github.com/marysoul99/A14.git
+
+:: Push the changes to the new repository
 git push -u origin main
